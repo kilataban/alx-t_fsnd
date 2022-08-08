6f968415142f2b6978489b30fee48f4bfeb4f785
Lydia Kila Taban <tabanlydia@gmail.com>
2022-08-08 22:58:02 +0300

    Insert new values into postgres via commandline

--- flask-hello-app.py
+++ flask-hello-app.py
@@ -3,9 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kilataban@localhost:5432/kilataban'
+
 db =SQLAlchemy(app)
+
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    person = Person.query.first()
+    return 'Hello ' + person.name
 
