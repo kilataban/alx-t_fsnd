c41faff3f0d2d987a2e254154386a0f255272995
Lydia Kila Taban <tabanlydia@gmail.com>
2022-08-08 20:42:49 +0300

    Specifly local db routes for flask app implementation

--- flask-hello-app.py
+++ flask-hello-app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kilataban@localhost:5432/kilataban'
+db =SQLAlchemy(app)
 
 @app.route('/')
 def index():
